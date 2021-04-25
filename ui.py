7493a1b8dbee55be717992d70821b54e05a6e2e2
Aniruddha V S <vsaniruddha@gmail.com>
2021-04-25 14:54:27 +0530

    "window"

--- ui.py
+++ ui.py
@@ -1,1 +1,13 @@
 import pygame
+
+pygame.init()
+width,height = 640,480
+screen=pygame.display.set_mode((width, height))
+while True:
+    screen.fill(255)
+    #screen.blit(, (100,100))
+    pygame.display.flip()
+    for event in pygame.event.get(): 
+        if event.type==pygame.QUIT:
+            pygame.quit() 
+            exit(0) 
