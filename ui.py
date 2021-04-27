94a779ed58b796a4e3706cd2fc6b020375537d1f
Aniruddha V S <vsaniruddha@gmail.com>
2021-04-27 10:40:16 +0530

    click and transperency

--- ui.py
+++ ui.py
@@ -3,11 +3,20 @@ import pygame
 pygame.init()
 width,height = 640,480
 screen=pygame.display.set_mode((width, height))
+pygame.display.set_caption("Wumpus World")
+flag = True
 while True:
-    screen.fill(255)
-    #screen.blit(, (100,100))
+    screen.fill((255,255,255))
+    newsurf = pygame.Surface((640,480))
+    newsurf.fill((0,0,0))
+    newsurf.set_alpha(50)
+    if flag:
+        screen.blit(newsurf,(0,0))
     pygame.display.flip()
     for event in pygame.event.get(): 
+        if event.type == pygame.MOUSEBUTTONUP:
+            print(event.pos)
+            flag = False
         if event.type==pygame.QUIT:
             pygame.quit() 
             exit(0) 
