9a68f1b995bcafb84889c7588c9db76867b32caf
Aniruddha V S <vsaniruddha@gmail.com>
2021-04-27 11:33:51 +0530

    Introduction

--- ui.py
+++ ui.py
@@ -4,6 +4,17 @@ pygame.init()
 width,height = 640,480
 screen=pygame.display.set_mode((width, height))
 pygame.display.set_caption("Wumpus World")
+fontH = pygame.font.Font('Inter-VariableFont_slnt,wght.ttf', 32)
+fontH2 = pygame.font.Font('Inter-VariableFont_slnt,wght.ttf', 26)
+fontH.set_bold(True)
+Welcome = fontH.render("Wumpus World",True,(0,0,0))
+fontH.set_bold(False)
+WelcomeRect = Welcome.get_rect()
+WelcomeRect.center = (width//2,height//3)
+Selectgrid = fontH2.render("Select A Grid:", True,(0,0,0))
+SelectgridRect = Selectgrid.get_rect()
+SelectgridRect.centerx = WelcomeRect.centerx
+SelectgridRect.top = WelcomeRect.bottom
 flag = True
 while True:
     screen.fill((255,255,255))
@@ -12,6 +23,8 @@ while True:
     newsurf.set_alpha(50)
     if flag:
         screen.blit(newsurf,(0,0))
+        screen.blit(Welcome,WelcomeRect)
+        screen.blit(Selectgrid,SelectgridRect)
     pygame.display.flip()
     for event in pygame.event.get(): 
         if event.type == pygame.MOUSEBUTTONUP:
